298f53924561a4eb700225d4bc70b65f473a4be2
Xiaoling Tang <aquatangtang@outlook.com>
2016-01-24 14:55:42 -0800

    The original commit for the password project

---
+++ assignment_1.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+	cout << "Hello World!\n\n";
+   cout << "My family name is Tang" << endl;
+   cout << "My student ID is 20264219" << endl << endl;
+   cout << "I noticed in the syllabus that the required student need to "
+        << "post at least one non-firvolous question, comment,or answer "
+        << "in the public forum every week.If you haven't post for two "
+        << "consecutive weeks, you will be dropped for non-participation."
+        << endl << endl;
+	return 0;
+}
+
+/*-------------------- paste of run ---------------------
+
+Hello World!
+
+My family name is Tang
+My student ID is 20264219
+
+I noticed in the syllabus that the required student need to post at least one non-firvolous question, comment,or answer in the public forum every week.If you haven't post for two consecutive weeks, you will be dropped for non-participation.
+
+Press any key to continue . . .
+
+---------------------------------------------------------*/
